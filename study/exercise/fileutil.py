d79118b51790bb6c520941d3904b6be6d1c1dcab
jiangfy <jiangfy@126.com>
2018-03-28 10:42:28 +0800

    初始化提交

--- study/exercise/fileutil.py
+++ study/exercise/fileutil.py
@@ -1,9 +1,12 @@
 # coding: UTF-8
 import os.path as path
+import os
 from exercise.common import show_title
 
 # 文件处理
 show_title('文件处理')
+
+
 def viewfile(filename):
     with open(filename) as f:
         print(f.read())
@@ -17,14 +20,33 @@ def view_lines(filename):
             i += 1
         # 重新读取文件
         f.seek(0)
-        lines=f.readlines()
-        func=lambda txt:'http' in txt
-        res=filter(func,lines)
+        lines = f.readlines()
+        func = lambda txt: 'http' in txt
+        res = filter(func, lines)
         for x in list(res):
             print(x.strip())
 
 
+# 文件系统扫描处理
+def print_files(path):
+    # 直接使用os.walk处理
+    for dirpath, dirnames, filenames in os.walk(path):
+        # print(dirpath)
+        # print('>',dirnames)
+        print('>>', filenames)
+    for dir in os.scandir(path):
+        if dir.is_dir():
+            print('d ', dir.name)
+        else:
+            print('f ', dir.name, dir.stat().st_size)
+
+
 if __name__ == '__main__':
-    filename = "/Users/jiangfy/Program/hivemq-3.3.2/conf/config.xml"
+    filename = os.environ['HOME'] + "/Program/hivemq-3.3.2/conf/config.xml"
     if path.exists(filename):
         view_lines(filename)
+    pdir = path.dirname(filename)
+    print_files(pdir)
+    print('系统环境变量')
+    for k, v in os.environ.items():
+        print(k, '=>', v)
